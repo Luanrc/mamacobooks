6a799c1046dcf02d1e453794ca3dcc4ccc9caf38
Luan <luan.rodrigues.c@gmail.com>
2021-11-19 07:53:59 -0300

    feat: create Authors controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  def index
+    render json: { message: 'entrou' } 
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+end
