91ca1582c3ea198d3c120b09d39d2e782041af68
Luan <luan.rodrigues.c@gmail.com>
2021-11-19 07:52:06 -0300

    feat: create routes to some endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ActiveAdmin.routes(self)
+  
+  resources :books
+  resources :publishers
+  resources :authors
+  resources :literares_genres
+
 end
